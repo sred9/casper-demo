3815ab7da8b669e20177fabe3936ccd7be1f7636
Sreehas Dommata <sreehasdommata98@gmail.com>
2020-05-04 17:04:33 +0530

    k

---
+++ src/styles/colors.ts
@@ -0,0 +1,15 @@
+export const colors = {
+  blue: '#3eb0ef',
+  green: '#a4d037',
+  purple: '#ad26b4',
+  yellow: '#fecd35',
+  red: '#f05230',
+  darkgrey: '#15171A',
+  midgrey: '#738a94',
+  lightgrey: '#7574ff',
+  whitegrey: '#e5eff5',
+  pink: '#fa3a57',
+  brown: '#a3821a',
+  // darkmode: color(var(--darkgrey) l(+2%)),
+  darkmode: '#12181b',
+};
